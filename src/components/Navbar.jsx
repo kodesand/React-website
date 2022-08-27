d68023d3bcbfbf4fbb295327df20594f7367fadb
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-27 17:21:45 +0800

    css variables

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link, NavLink } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
+
+import { links } from './data.js'
+import Logo from '/images/logo.png'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='container nav__container'>
+        <Link to='/' className='logo'>
+          <img src={Logo} alt='Nav Logo' />
+        </Link>
+        <ul className='nav__links'>
+          {links.map(({ name, path }, index) => {
+            return (
+              <li key={index}>
+                <NavLink to={path}>{name}</NavLink>
+              </li>
+            )
+          })}
+        </ul>
+        <button className='nav__toggle-btn'>
+          <FaBars />
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
