719c24e6254ad4a453bd223f94738d332730b435
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-29 03:04:02 +0800

    testimonial component functionality

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -8,12 +8,27 @@ import {
 import SectionHeader from '@/components/SectionHeader'
 import Card from '@/UI/card'
 
-import data from './data.js'
+import testimonials from './data.js'
 import './style.css'
 
 const Testimonials = () => {
   const [index, setIndex] = useState(0)
-  const { name, quote, job, avatar } = data[index]
+  const { name, quote, job, avatar } = testimonials[index]
+
+  const prevTestimonialHandler = () => {
+    setIndex((prev) => prev - 1)
+
+    if (index <= 0) {
+      setIndex(testimonials.length - 1)
+    }
+  }
+  const nextTestimonialHandler = () => {
+    setIndex((prev) => prev + 1)
+
+    if (index >= testimonials.length - 1) {
+      setIndex(0)
+    }
+  }
 
   return (
     <section className='testimonials'>
@@ -32,10 +47,16 @@ const Testimonials = () => {
           <small className='testimonial__title'>{job}</small>
         </Card>
         <div className='testimonials__btn-container'>
-          <button className='testimonials__btn'>
+          <button
+            className='testimonials__btn'
+            onClick={prevTestimonialHandler}
+          >
             <IoIosArrowDropleftCircle />
           </button>
-          <button className='testimonials__btn'>
+          <button
+            className='testimonials__btn'
+            onClick={nextTestimonialHandler}
+          >
             <IoIosArrowDroprightCircle />
           </button>
         </div>
