9b5e598dfbc4f917cb1c86810efb3b47cf07252f
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-30 16:49:31 +0800

    gallery page structure

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,5 +1,36 @@
+import Header from '@/components/Header'
+import HeaderImage from '/images/header_bg_3.jpg'
+
+import './gallery.css'
+
 const Gallery = () => {
-  return <div>Gallery</div>
+  const galleryLength = 15
+  const images = []
+
+  for (let index = 1; index <= galleryLength; index++) {
+    images.push(`/images/gallery${index}.jpg`)
+  }
+  console.log(images)
+
+  return (
+    <>
+      <Header title='Our Gallery' image={HeaderImage}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad totam
+        maiores eos exercitationem expedita consectetur!
+      </Header>
+      <section className='gallery'>
+        <div className='container gallery__container'>
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            )
+          })}
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Gallery
