5bc1b3943c00271fb5cb5288551c9ecf5bcab55f
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-09-01 00:17:34 +0800

    fix icons shape from footer and contact

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import Logo from '/images/logo.png'
-import { FaLinkedin, FaFacebookF } from 'react-icons/fa'
+import { FaLinkedin, FaFacebookF, FaTiktok, FaShopify } from 'react-icons/fa'
 import { AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai'
 
 import './style.css'
@@ -19,56 +19,57 @@ const Footer = () => {
           </p>
           <div className='footer__socials'>
             <a
-              href='https://linkedin.com'
-              target='_blank'
-              rel='noreferrer noopener'
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href='https://FACEBOOK.com'
+              href='https://facebook.com/tatakrgb'
               target='_blank'
               rel='noreferrer noopener'
             >
               <FaFacebookF />
             </a>
             <a
-              href='https://twitter.com'
-              target='_blank'
-              rel='noreferrer noopener'
-            >
-              <AiOutlineTwitter />
-            </a>
-            <a
-              href='https://instagram.com'
+              href='https://instagram.com/rgbleathercrafts'
               target='_blank'
               rel='noreferrer noopener'
             >
               <AiFillInstagram />
             </a>
+            <a
+              href='https://tiktok.com/@tatakrgb'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <FaTiktok />
+            </a>
+            <a
+              href='https://shp.ee/9gzr4gx'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <FaShopify />
+            </a>
           </div>
         </article>
-        <article>
-          <h4>Permalinks</h4>
-          <Link to='/about'>About</Link>
-          <Link to='/plans'>Plans</Link>
-          <Link to='/trainers'>Trainers</Link>
-          <Link to='/gallery'>Gallery</Link>
-          <Link to='/contact'>Contact</Link>
-        </article>
-        <article>
-          <h4>Insights</h4>
-          <Link to='/s'>Blog</Link>
-          <Link to='/s'>Case Studies</Link>
-          <Link to='/s'>Events</Link>
-          <Link to='/s'>Communities</Link>
-          <Link to='/s'>FAQs</Link>
-        </article>
-        <article>
-          <h4>Get In Touch</h4>
-          <Link to='/contact'>Contact Us</Link>
-          <Link to='/s'>Support</Link>
-        </article>
+        <div className='permalinks'>
+          <article>
+            <h4>Permalinks</h4>
+            <Link to='/about'>About</Link>
+            <Link to='/gallery'>Gallery</Link>
+            <Link to='/products'>Products</Link>
+            <Link to='/contact'>Contact</Link>
+          </article>
+          <article>
+            <h4>Insights</h4>
+            <Link to='/s'>Blog</Link>
+            <Link to='/s'>Case Studies</Link>
+            <Link to='/s'>Events</Link>
+            <Link to='/s'>Communities</Link>
+            <Link to='/s'>FAQs</Link>
+          </article>
+          <article>
+            <h4>Get In Touch</h4>
+            <Link to='/contact'>Contact Us</Link>
+            <Link to='/s'>Support</Link>
+          </article>
+        </div>
       </div>
       <div className='footer__copyright'>
         <small>2022 RGB LeatherCrafts &copy; All rights Reserved</small>
